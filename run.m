be212fd50c964244dc0ff355e56f36113fda20ff
Hossain and Troy <ami.masroor@hotmail.com>
2017-07-03 14:07:43 -0500

    Sample.wav is reduced in size

--- run.m
+++ run.m
@@ -1,5 +1,6 @@
 % getitng audio samples to play back 
-% at different pitches by doing an FFT -> shifting the frequency bins -> IFFT
+% at different pitches by doing an FFT -> shifting the frequency bins 
+%-> IFFT
 
 
 y = audioread("sample.wav");
@@ -9,16 +10,17 @@ y = audioread("sample.wav");
 y_fft = fft(y);
 size(y_fft)
 
+
 %array shift of indice
 n =2;
 cnstnt = 1.059;
 
 for i=1:length (y_fft)
-  shift_y(i) = round(cnstnt*n*i);
+  shift_y(i) = round(cnstnt*n*i);  
 %  content of shit_y has transformed indices
   endfor
   
 for i=1:length (y_fft)
-   out(shift_y(i)) = y_fft(i);
+   out(shift_y(i)) = y_fft(i)
    endfor
    
